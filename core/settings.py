a1363ad8c509df02034fef72fe041c6957e92b81
Karan Modi <111880507+karan-trootech@users.noreply.github.com>
2023-01-02 15:46:16 +0530

    [ deleted vercel config trying railway now ]

--- core/settings.py
+++ core/settings.py
@@ -188,4 +188,4 @@ SIMPLE_CHATBOT = {
 
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles_build','static')
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
