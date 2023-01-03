19684135c9b355b623bf12dfe52c10095df33ff6
Karan Modi <111880507+karan-trootech@users.noreply.github.com>
2023-01-03 16:58:41 +0530

    [ error log to find and debug celery not working ]

--- bookMyShowApp/tasks.py
+++ bookMyShowApp/tasks.py
@@ -2,7 +2,7 @@ from celery import shared_task
 from .models import Booking,ErrorLog
 from datetime import datetime
 import pytz
-
+import json
 
 
 @shared_task(name='revert_seats_func')
@@ -15,8 +15,13 @@ def test_func():
     checking_list = []
     for i in range(len(bookings)):
         booking_obj = bookings[i]
-        obj = ErrorLog.objects.create({"error":"True","currentime":f"{date_time_current}",
-                                       "expiretime":str(booking_obj.get("seat_block_duration"))})
+        obj = ErrorLog.objects.create()
+        json_field = obj.error_data
+        json_field["error"]="True"
+        json_field["currentime"]=f"{date_time_current}"
+        json_field["expiretime"]=str(booking_obj.get("seat_block_duration"))
+        obj.save()
+        
         if date_time_current > booking_obj.get("seat_block_duration"):
             obj = Booking.objects.get(id=booking_obj.get("id"))
             if obj.payment_status != True:
